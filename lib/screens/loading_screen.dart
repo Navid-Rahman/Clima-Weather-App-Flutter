9947789af102fb9916d5e5c4b57f5bb690d2361d
Navid <navidrahman92@gmail.com>
2023-06-19 15:29:25 +0600

    API parsing data successful

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
@@ -16,7 +20,19 @@ class _LoadingScreenState extends State<LoadingScreen> {
     }
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.low);
-    print(position);
+
+    // HTTP request
+    const apiKey = 'cacfefb9b486fea21a063f79e5c9ebc6';
+    final apiUrl =
+        'https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=$apiKey';
+    final response = await http.get(Uri.parse(apiUrl));
+
+    if (response.statusCode == 200) {
+      final weatherData = json.decode(response.body);
+      print(weatherData);
+    } else {
+      throw Exception('Failed to fetch weather data');
+    }
   }
 
   @override
